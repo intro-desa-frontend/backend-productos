11c34857f4188396096e06cb706fc6c0b435c507
Ruben Romero <romeroa.ruben@gmail.com>
2023-10-22 13:50:24 -0300

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -7,11 +7,6 @@ const { auth } = require("express-oauth2-jwt-bearer");
 
 require("dotenv").config();
 
-const oauthCheck = auth({
-  audience: process.env.OAUTH_AUDIENCE,
-  issuerBaseURL: process.env.OAUTH_URL,
-  tokenSigningAlg: "RS256",
-});
 
 const app = express();
 app.use(express.json())
